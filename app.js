1b4ec83a808f3c86c61eb41ecd809202cf1abeb5
Anthony Lapham <lapham.2@wright.edu>
2018-12-23 08:36:14 -0500

    minor updates

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
 const request = require('request');
 const mongoose = require('mongoose');
-const body-parser = require('body-parser');
+const bodyParser = require('body-parser');
 
-mongoose.connect(process.env.DB_URI, {
+/*mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true
-});
+});*/
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+/*const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));*/
 
 request({
-  url: db,
+  url:db,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results.providedLocation}`);
+  //console.log(`Longitude: ${body.results.latLng.lng}`);
+  //console.log(`Latitude: ${body.results.latling.lat}`);
 });
