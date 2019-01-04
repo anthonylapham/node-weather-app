c43eebe6a9b7c87563015df8b8cdf829dd17a841
Anthony Lapham <lapham.2@wright.edu>
2019-01-04 09:16:22 -0500

    removed key

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const request = require('request');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const yargs = require('yargs');
+const postcode = require('postcode-validator');
 
 const argv = yargs
   .options({
@@ -16,7 +17,7 @@ const argv = yargs
   })
   .help().alias('help', 'h').argv;
 
-  const encodedAddress = encodeURIComponent(argv.address);
+const encodedAddress = encodeURIComponent(argv.address);
 
 /*mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true
@@ -26,11 +27,19 @@ const argv = yargs
 db.on('error', console.error.bind(console, 'connection error:'));*/
 
 request({
-  url: `http://www.mapquestapi.com/geocoding/v1/address=${encodedAddress}`,
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=${process.env.KEY}&location=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   const [results] = body.results;
-  console.log(`Address: ${results.providedLocation.location}`);
-  console.log(`Longitude: ${results.locations[0].latLng.lng}`);
-  console.log(`Latitude: ${results.locations[0].latLng.lat}`);
+  if (error) {
+    console.log("Unable to connect to Google servers");
+  } else if (results.locations[0].street === "") {
+    console.log("Unable to find location");
+  } else {
+    const [results] = body.results;
+    console.log(`Address: ${results.providedLocation.location}`);
+    console.log(`Longitude: ${results.locations[0].latLng.lng}`);
+    console.log(`Latitude: ${results.locations[0].latLng.lat}`);
+  }
+
 });
